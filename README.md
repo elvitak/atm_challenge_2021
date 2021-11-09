2d9a4e22eb2d5a49e6060f77772e68b7de1a0c9e
DefyCab <love.bellinder@gmail.com>
2021-11-09 10:12:16 +0100

    define instance_double pass test

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 As a user
 In order to make a withdrawal 
 ATM needs to have funds
+
+As a User
+In order to know if my withdrawal was succesful or unsuccessful
+I want to recieve a message with my withdrawal details
+
+As an ATM operator
+In order for the right person to make a withdrawal
+I want each person to have a separate account
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -3,6 +3,13 @@ require './lib/atm.rb'
 
 describe Atm do 
     subject { Atm.new } # can be omitted
+    let (:account) { instance_double('Account')}
+
+    before do
+        allow(account).to receive(:balance).and_return(100)
+        allow(account).to receive(:balance=)
+    end
+
     it 'is expected to hold $1000 when instantiated' do 
         expect(subject.funds).to eq 1000 
     end
