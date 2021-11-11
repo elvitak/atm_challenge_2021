8a87f3701dc443c86655ba114a9adbe82537c8a0
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-11-11 16:16:34 +0100

    create Account class in Person class and test it

--- lib/person.rb
+++ lib/person.rb
@@ -1,31 +1,26 @@
-class Person 
+require './lib/account'
+class Person
+  attr_accessor :name, :cash, :account
 
-attr_accessor :name, :cash, :account
-
-def initialize(attrs = {})
+  def initialize(attrs = {})
     @name = set_name(attrs[:name])
     @cash = 0
     @account = nil
-end
+  end
 
-
-
-def set_name(name)
+  def set_name(name)
     name == nil ? missing_name : name
-end
+  end
 
-def missing_name
-    raise ArgumentError, "A name is required"
-end
+  def missing_name
+    raise ArgumentError, 'A name is required'
+  end
 
-def missing_account
-    raise RuntimeError, "No account present"
-end
+  def missing_account
+    raise RuntimeError, 'No account present'
+  end
 
-def create_account
+  def create_account
     @account = Account.new(owner: self)
+  end
 end
-
-
-end
-
