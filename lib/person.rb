035b14c5f1161698a3e61e7c58d22590381da44d
gsim1903 <gavin@internetbusker.co>
2021-11-11 14:13:28 +0100

    define  missing name, missing account and create account. Debugging RB 26 in person_spec

--- lib/person.rb
+++ lib/person.rb
@@ -4,13 +4,28 @@ attr_accessor :name, :cash, :account
 
 def initialize(attrs = {})
     @name = set_name(attrs[:name])
-    @cash = :active
+    @cash = 0
     @account = nil
 end
+
+
 
 def set_name(name)
     name == nil ? missing_name : name
 end
+
+def missing_name
+    raise ArgumentError, "A name is required"
+end
+
+def missing_account
+    raise RuntimeError, "No account present"
+end
+
+def create_account
+    @account = Account.new(owner: self)
+end
+
 
 end
 
