c739ee5650d89cb72b02360b712ad2a3e4b06d42
gsim1903 <gavin@internetbusker.co>
2021-11-11 11:48:23 +0100

    add test of account status to deactivate using instance method

--- lib/account.rb
+++ lib/account.rb
@@ -2,13 +2,27 @@ require 'date'
 
 class Account
   STANDART_VALIDITY_YRS = 5
-  attr_accessor :exp_date
+  attr_accessor :exp_date, :account_status
 
   def initialize
     @exp_date = set_exp_date
+    @account_status = :active
   end
 
   def set_exp_date
     Date.today.next_year(STANDART_VALIDITY_YRS).strftime('%m/%y')
   end
+
+  #def self.deactivate(account)
+   # account.account_status = :deactivated 
+   #end
+
+  def deactivate
+    @account_status = :deactivated
+  end
+
+
+
+
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,7 @@
 require './lib/account.rb'
 RSpec.describe Account do
   subject { described_class.new }
+  
 
   it 'is expected to check the length of a number' do
     number = 1234
@@ -12,4 +13,19 @@ RSpec.describe Account do
     expected_date = Date.today.next_year(5).strftime('%m/%y')
     expect(subject.set_exp_date).to eq expected_date
   end
+
+  it 'is expected to have :active status on initialize' do 
+    expect(subject.account_status).to eq :active 
+    end
+
+  #it 'is expected to deactivate the account using the class method' do
+   # Account.deactivate(subject)
+    #expect(subject.account_status). to eq :deactivated
+  #end
+
+  it 'is expected to deactivate the account using the instance method' do
+  subject.deactivate 
+  expect(subject.account_status). to eq :deactivated
+  end
+
 end
