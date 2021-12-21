06b562b4b0fd8a649cf293ade9a48b1867ccdc8b
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-21 17:45:27 +0100

    add withdraw method to person class

--- lib/person.rb
+++ lib/person.rb
@@ -8,6 +8,20 @@ class Person
     @account = nil
   end
 
+  def deposit(amount)
+    @account == nil ? missing_account : deposit_funds(amount)
+  end
+
+  def create_account
+    @account = Account.new(owner: self)
+  end
+
+  def withdraw(args = {})
+    @account == nil ? missing_account : withdraw_funds(amount)
+  end
+
+  private
+
   def set_name(name)
     name == nil ? missing_name : name
   end
@@ -16,13 +30,6 @@ class Person
     raise ArgumentError, 'A name is required'
   end
 
-  def create_account
-    @account = Account.new(owner: self)
-  end
-
-  def deposit(amount)
-    @account == nil ? missing_account : deposit_funds(amount)
-  end
 
   def missing_account
     raise RuntimeError, 'No account present'
@@ -31,5 +38,14 @@ class Person
   def deposit_funds(amount)
     @cash -= amount
     @account.balance += amount
+  end
+
+  def withdraw_funds(args)
+    args[:atm] == nil ? missing_atm : atm = args[:atm]
+    account = @account
+    amount = arggs[:amount]
+    pin = args[:pin]
+    response = atm.withdraw(amount, pin, account)
+    response[:status] = true ? increase_cash(response) :response
   end
 end
